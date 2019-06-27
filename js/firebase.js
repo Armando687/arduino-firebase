1de60d2d8fa3ab540c8cd5bfd03cd83bb1d49947
Armando687 <jlopezrodriguez687@gmail.com>
2019-06-27 10:28:57 -0400

    problema de recardado de pagina

---
+++ js/firebase.js
@@ -0,0 +1,45 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAtobkS6J9vl1kUKT5RHxUsQ0FFNCj_DHY",
+    authDomain: "proyectoinicio-aa3c3.firebaseapp.com",
+    databaseURL: "https://proyectoinicio-aa3c3.firebaseio.com",
+    projectId: "proyectoinicio-aa3c3",
+    storageBucket: "proyectoinicio-aa3c3.appspot.com",
+    messagingSenderId: "745203185708",
+    appId: "1:745203185708:web:5700a02273e3bdea"
+  };
+  firebase.initializeApp(firebaseConfig);
+
+   // Get a reference to the database ser
+   function writeTemperatura() {
+       firebase.database().ref('home/' + "Temperatura").set({
+         temperatura: "14",
+         estado: "2"
+       });
+     }
+writeTemperatura();
+  function readTemperatura(){
+    var result = null;
+    var temperatura = firebase.database().ref('home/Temperatura');
+    temperatura.on('value',function(data){
+      result = data.val();
+      console.log(result.temperatura);
+      $("#temperatura").text(result.temperatura);
+      if(result.estado == 1){
+        $("#estado").text("Normal");
+      }else{
+        $("#estado").removeClass("badge badge-primary badge-pill");
+        $("#estado").addClass("badge badge-danger badge-pill");
+        $("#estado").text("Critico");
+      }
+      if(result != null){
+        $("#conexion").text("Activo");
+      }else{
+        $("#conexion").text("Inactivo");
+      }
+      
+    });
+    
+  }
+readTemperatura();
+ 
